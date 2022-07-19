4a69ef832988331940394d261dc38098f50f013f
K. Wittenberg <kejo@net.hr>
2022-07-19 16:30:53 +0200

    Add project files.

---
+++ Ispit.Todo/GlobalUsing.cs
@@ -0,0 +1,28 @@
+﻿global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+global using Microsoft.AspNetCore.Mvc.Rendering;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Options;
+
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Diagnostics;
+global using System.Security.Claims;
+
+
+global using Ispit.Todo.Data;
+global using Ispit.Todo.Models;
+//global using Ispit.Todo.Models.Base;
+//global using Ispit.Todo.Models.Binding;
+//global using Ispit.Todo.Models.Dbo;
+//global using Ispit.Todo.Models.Dbo.Base;
+//global using Ispit.Todo.Models.Dto;
+//global using Ispit.Todo.Models.ViewModel;
+             
+//global using Ispit.Todo.Services.Interface;
+//global using Ispit.Todo.Services.Implementation;
+
+//global using AutoMapper;
